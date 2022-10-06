a17938d4dbeb5c5d49d3cba66d231fba5e1d2d0d
AngeloCalabro <angelo.1991@hotmail.com>
2022-10-06 18:04:13 +0200

    ticket con sconti

--- js/script.js
+++ js/script.js
@@ -21,8 +21,8 @@ let writeDate = gg + mm + aaaa;
 console.log(writeDate)
 document.getElementById('date_add').innerHTML = writeDate;
 
-// let kmUtente = parseInt(prompt("Inserire il numero di km da percorrere"));
-// let etaUtente = parseInt(prompt("Inserire l'età del passeggero"));
+let kmUtente = parseInt(prompt("Inserire il numero di km da percorrere"));
+let etaUtente = parseInt(prompt("Inserire l'età del passeggero"));
 console.log(kmUtente + ` km`, etaUtente + ` età`);
 
 document.getElementById('km_around').innerHTML = kmUtente;
@@ -38,20 +38,26 @@ const kmEuroPercorsi = parseFloat(prezzoKm * kmUtente);
 
 const minorenne = 18;
 const over_65 = 65;
+
 if (etaUtente < minorenne) {
     const sconto_20 = ((kmEuroPercorsi / 100) * 20);
     console.log('sconto 20%')
     let prezzoScontato_20 = kmEuroPercorsi - sconto_20
     console.log(new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(prezzoScontato_20));
+    document.getElementById('add_discount').innerHTML = `Ti è stato applicato uno sconto del 20% dal listino`;
+    document.getElementById('total_price').innerHTML = `Tot: ` + new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(prezzoScontato_20);
 
 } else if (etaUtente > over_65) {
     const sconto_40 = ((kmEuroPercorsi / 100) * 40);
     console.log('sconto 40%')
     let prezzoScontato_40 = kmEuroPercorsi - sconto_40
     console.log(new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(prezzoScontato_40));
+    document.getElementById('add_discount').innerHTML = `Ti è stato applicato uno sconto del 40% dal listino`;
+    document.getElementById('total_price').innerHTML = `Tot: ` + new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(prezzoScontato_40);
 
 } else {
     console.log('no sconto')
     console.log(new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(kmEuroPercorsi));
+    document.getElementById('total_price').innerHTML = `Tot: ` + new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(kmEuroPercorsi);
 
 }
